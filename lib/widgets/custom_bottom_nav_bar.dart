5b171ac7fd90a487d84ff3f9f47d9fc30cfb5527
Reema Asker <reema1822000@gmail.com>
2025-02-01 12:56:47 +0200

    bottom_nav updated

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -1,5 +1,5 @@
 
-import 'package:doctor/screens/specialist/applicationInfo.dart';
+import 'package:doctor/screens/applicationInfo.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
