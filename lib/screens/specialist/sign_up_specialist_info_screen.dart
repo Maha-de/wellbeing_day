b74fe7c64c1fd29c02b912f47ec63d7c929d603a
Reema Asker <reema1822000@gmail.com>
2025-02-14 14:08:45 +0200

    sp done

--- lib/screens/specialist/sign_up_specialist_info_screen.dart
+++ lib/screens/specialist/sign_up_specialist_info_screen.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:doctor/cubit/get_specialist/get_sepcialist_cubit.dart';
 import 'package:doctor/cubit/get_specialist/get_specialist_state.dart';
 import 'package:doctor/screens/specialist/repo/SpecialistRepository.dart';
-import 'package:doctor/screens/specialist/sign_up_specialist_spcalize_screen.dart.dart';
+import 'package:doctor/screens/specialist/sign_up_specialist_spcalize_screen.dart';
 import 'package:doctor/widgets/custom_text_field_for_sign_up.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:file_picker/file_picker.dart';
@@ -58,9 +58,7 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
   final TextEditingController associationMembershipController =
       TextEditingController();
 
-
   late GetSpecialistCubit getSpecialistCubit;
-
 
   @override
   void initState() {
@@ -92,7 +90,7 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                     child: Text(
                       "headerAsSpecialist".tr(),
                       textAlign: TextAlign.center,
-                      style:  TextStyle(
+                      style: TextStyle(
                         fontSize: 20.sp,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
@@ -100,58 +98,87 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                       ),
                     ),
                   ),
-                   SizedBox(height: 32.h),
+                  SizedBox(height: 32.h),
                   CustomTextField(
                     label: "firstName".tr(),
-                    controller: context.read<GetSpecialistCubit>().firstNameController,
-                    validator: (value) =>
-                        isSubmitted ? validateName(value, "firstName".tr()) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().firstNameController,
+                    validator: (value) => isSubmitted
+                        ? validateName(value, "firstName".tr())
+                        : null,
                   ),
                   CustomTextField(
                     label: "lastName".tr(),
-                    controller: context.read<GetSpecialistCubit>().lastNameController,
-                    validator: (value) =>
-                        isSubmitted ? validateName(value, "lastName".tr(),) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().lastNameController,
+                    validator: (value) => isSubmitted
+                        ? validateName(
+                            value,
+                            "lastName".tr(),
+                          )
+                        : null,
                   ),
-
-
                   CustomTextField(
                     label: "email".tr(),
-                    controller: context.read<GetSpecialistCubit>().emailController,
-                    validator: (value) => isSubmitted ? validateEmail(value, "email".tr(),) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().emailController,
+                    validator: (value) => isSubmitted
+                        ? validateEmail(
+                            value,
+                            "email".tr(),
+                          )
+                        : null,
                     suffixIcon: Icons.mail,
                   ),
                   CustomTextField(
                     label: "password".tr(),
-                    controller: context.read<GetSpecialistCubit>().passwordController,
-                    validator: (value) => isSubmitted ? validatePassword(value, "password".tr()) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().passwordController,
+                    validator: (value) => isSubmitted
+                        ? validatePassword(value, "password".tr())
+                        : null,
                     suffixIcon: Icons.remove_red_eye,
                   ),
                   CustomTextField(
                     label: "confirmPassword".tr(),
-                    controller: context.read<GetSpecialistCubit>().confirmPasswordController,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .confirmPasswordController,
                     validator: isSubmitted
                         ? (value) => validateConfirmPassword(
-                            value, context.read<GetSpecialistCubit>().passwordController.text)
+                            value,
+                            context
+                                .read<GetSpecialistCubit>()
+                                .passwordController
+                                .text)
                         : null,
                     suffixIcon: Icons.remove_red_eye,
                   ),
                   CustomTextField(
                     keyboardType: TextInputType.number,
                     label: "phoneNumber".tr(),
-                    controller: context.read<GetSpecialistCubit>().phoneController,
-                    validator: (value) => isSubmitted ? validatePhone(value, "phoneNumber".tr()) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().phoneController,
+                    validator: (value) => isSubmitted
+                        ? validatePhone(value, "phoneNumber".tr())
+                        : null,
                     suffixIcon: Icons.phone_android,
                   ),
                   CustomTextField(
                     label: "nationality".tr(),
-                    controller: context.read<GetSpecialistCubit>().nationalityController,
-                    validator: (value) => isSubmitted ? validateNationality(value, "nationality".tr()) : null,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .nationalityController,
+                    validator: (value) => isSubmitted
+                        ? validateNationality(value, "nationality".tr())
+                        : null,
                     suffixIcon: Icons.person,
                   ),
                   CustomTextField(
                     label: "homeAddress".tr(),
-                    controller: context.read<GetSpecialistCubit>().homeAddressController,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .homeAddressController,
                     validator: isSubmitted
                         ? (value) => validateAddress(value, "homeAddress".tr())
                         : null,
@@ -159,7 +186,8 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                   ),
                   CustomTextField(
                     label: "profession".tr(),
-                    controller: context.read<GetSpecialistCubit>().workController,
+                    controller:
+                        context.read<GetSpecialistCubit>().workController,
                     validator: isSubmitted
                         ? (value) => validateName(value, "profession".tr())
                         : null,
@@ -167,16 +195,21 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                   ),
                   CustomTextField(
                     label: "professionAddress".tr(),
-                    controller: context.read<GetSpecialistCubit>().workAddressController,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .workAddressController,
                     validator: isSubmitted
-                        ? (value) => validateAddress(value, "professionAddress".tr())
+                        ? (value) =>
+                            validateAddress(value, "professionAddress".tr())
                         : null,
                     suffixIcon: Icons.location_city,
                   ),
                   CustomTextField(
                     label: "bio".tr(),
                     isMultiline: true,
-                    controller: context.read<GetSpecialistCubit>().about_doctor_Controller,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .about_doctor_Controller,
                     validator: isSubmitted
                         ? (value) => validateName(value, "bio".tr())
                         : null,
@@ -185,35 +218,41 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                   CustomTextField(
                     keyboardType: TextInputType.number,
                     label: "experience".tr(),
-                    controller: context.read<GetSpecialistCubit>().exp_year_Controller,
-                    validator: (value) => isSubmitted ? validateYearsOfExperience(value, "experience".tr()) : null,
+                    controller:
+                        context.read<GetSpecialistCubit>().exp_year_Controller,
+                    validator: (value) => isSubmitted
+                        ? validateYearsOfExperience(value, "experience".tr())
+                        : null,
                     suffixIcon: Icons.work_history,
                   ),
                   CustomTextField(
                     keyboardType: TextInputType.number,
                     label: "sessionDuration".tr(),
-                    controller: context.read<GetSpecialistCubit>().session_time_Controller,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .session_time_Controller,
                     validator: isSubmitted
-                        ? (value) => validateSessionDetails(value, "sessionDuration".tr())
+                        ? (value) => validateSessionDetails(
+                            value, "sessionDuration".tr())
                         : null,
                     suffixIcon: Icons.timer,
                   ),
                   CustomTextField(
                     keyboardType: TextInputType.number,
                     label: "sessionPrice".tr(),
-                    controller: context.read<GetSpecialistCubit>().session_price_Controller,
+                    controller: context
+                        .read<GetSpecialistCubit>()
+                        .session_price_Controller,
                     validator: isSubmitted
-                        ? (value) => validateSessionDetails(value, "sessionPrice".tr())
+                        ? (value) =>
+                            validateSessionDetails(value, "sessionPrice".tr())
                         : null,
                     suffixIcon: Icons.price_check_sharp,
                   ),
-
-
                   CustomUploadFile(
                     label: "cv".tr(),
                     controller: resumeController,
-                    validator: (value) =>
-                        validateUpload(value, "cv".tr()),
+                    validator: (value) => validateUpload(value, "cv".tr()),
                     onFilePicked: (PlatformFile? file) {
                       if (file != null) {
                         cvFile = file;
@@ -241,7 +280,8 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                   CustomUploadFile(
                     label: "certificates".tr(),
                     controller: certificatesController,
-                    validator: (value) => validateUpload(value, "certificates".tr()),
+                    validator: (value) =>
+                        validateUpload(value, "certificates".tr()),
                     onFilePicked: (PlatformFile? file) {
                       if (file != null) {
                         certificates = file;
@@ -252,8 +292,7 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                   ),
                   CustomUploadFile(
                     label: "license".tr(),
-                    validator: (value) =>
-                        validateUpload(value, "license".tr()),
+                    validator: (value) => validateUpload(value, "license".tr()),
                     controller: licenseController,
                     onFilePicked: (PlatformFile? file) {
                       if (file != null) {
@@ -284,21 +323,59 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                       });
 
                       if (signUpFormKey.currentState?.validate() ?? false) {
-                        Doctor doctor = Doctor.withoutSpeciality(
-                          phone: context.read<GetSpecialistCubit>().phoneController.text,
-                          firstName: context.read<GetSpecialistCubit>().firstNameController.text,
-                          lastName: context.read<GetSpecialistCubit>().lastNameController.text,
-                          email: context.read<GetSpecialistCubit>().emailController.text,
-                          password: context.read<GetSpecialistCubit>().passwordController.text,
-                          nationality: context.read<GetSpecialistCubit>().nationalityController.text,
-                          work: context.read<GetSpecialistCubit>().workController.text,
-                          workAddress: context.read<GetSpecialistCubit>().workAddressController.text,
-                          homeAddress: context.read<GetSpecialistCubit>().homeAddressController.text,
-                          bio: context.read<GetSpecialistCubit>().about_doctor_Controller.text,
-                          yearOfExperience:
-                              int.tryParse(context.read<GetSpecialistCubit>().exp_year_Controller.text)!,
-                          sessionPrice: context.read<GetSpecialistCubit>().session_price_Controller.text,
-                          sessionDuration: context.read<GetSpecialistCubit>().session_time_Controller.text,
+                        Specialist doctor = Specialist.withoutSpeciality(
+                          phone: context
+                              .read<GetSpecialistCubit>()
+                              .phoneController
+                              .text,
+                          firstName: context
+                              .read<GetSpecialistCubit>()
+                              .firstNameController
+                              .text,
+                          lastName: context
+                              .read<GetSpecialistCubit>()
+                              .lastNameController
+                              .text,
+                          email: context
+                              .read<GetSpecialistCubit>()
+                              .emailController
+                              .text,
+                          password: context
+                              .read<GetSpecialistCubit>()
+                              .passwordController
+                              .text,
+                          nationality: context
+                              .read<GetSpecialistCubit>()
+                              .nationalityController
+                              .text,
+                          work: context
+                              .read<GetSpecialistCubit>()
+                              .workController
+                              .text,
+                          workAddress: context
+                              .read<GetSpecialistCubit>()
+                              .workAddressController
+                              .text,
+                          homeAddress: context
+                              .read<GetSpecialistCubit>()
+                              .homeAddressController
+                              .text,
+                          bio: context
+                              .read<GetSpecialistCubit>()
+                              .about_doctor_Controller
+                              .text,
+                          yearOfExperience: int.tryParse(context
+                              .read<GetSpecialistCubit>()
+                              .exp_year_Controller
+                              .text)!,
+                          sessionPrice: context
+                              .read<GetSpecialistCubit>()
+                              .session_price_Controller
+                              .text,
+                          sessionDuration: context
+                              .read<GetSpecialistCubit>()
+                              .session_time_Controller
+                              .text,
                           idOrPassport: idOrPassport,
                           resume: cvFile,
                           certificates: certificates,
@@ -335,7 +412,7 @@ class _SignUpAsDoctorFirstScreenState extends State<SignUpAsDoctorFirstScreen> {
                       child: Center(
                         child: Text(
                           "next".tr(),
-                          style:  TextStyle(
+                          style: TextStyle(
                             fontSize: 18.sp,
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
